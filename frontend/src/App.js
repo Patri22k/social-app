e47c7cb103f1df6219054fa33092a25f4689f92e
Patri22k <bajzik.patrik6@gmail.com>
2024-02-18 15:37:34 +0100

    Create frontend structure

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,10 +2,18 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1 className="text-9xl font-bold underline">
-        Hello world!
-      </h1>
+    <div className="App flex h-screen">
+      <div className="chatHistory h-screen w-1/3">
+
+      </div>
+      <div className="chatContainer flex flex-col h-screen w-2/3">
+        <div className="chatDisplay h-4/5">
+
+        </div>
+        <div className="chatInput h-1/5">
+          <input type="text" className="w-[calc(100%-1rem)] h-12   border-2 rounded-3xl" />
+        </div>
+      </div>
     </div>
   );
 }
