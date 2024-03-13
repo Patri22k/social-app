02b205c0607a54000125b775ba7e0d27f4383898
Patri22k <bajzik.patrik6@gmail.com>
2024-03-13 12:45:10 +0100

    Add proper comments in authRouter.js

--- packages/backend/routhers/authRouter.js
+++ packages/backend/routhers/authRouter.js
@@ -1,11 +1,30 @@
+/**
+ * Express router for handling authentication routes.
+ * @module authRouter
+ */
+
 const express = require('express');
 const router = express.Router();
 const formVerification = require('../controlers/formVerification');
 
+/**
+ * Route for user login.
+ * @name POST /login
+ * @function
+ * @param {Object} req - Express request object
+ * @param {Object} res - Express response object
+ */
 router.post('/login', (req, res) => {
   formVerification(req, res);
 });
 
+/**
+ * Route for user signup.
+ * @name POST /signup
+ * @function
+ * @param {Object} req - Express request object
+ * @param {Object} res - Express response object
+ */
 router.post('/signup', (req, res) => {
   formVerification(req, res);
 });
