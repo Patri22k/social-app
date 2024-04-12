d2df11a58f8b0f266118b33a08e412dbeb907e2d
Patri22k <bajzik.patrik6@gmail.com>
2024-04-12 10:17:42 +0200

    Refactor RestrictedContent component to improve readability and add comments

--- packages/frontend/src/components/Auth/RestrictedContent.js
+++ packages/frontend/src/components/Auth/RestrictedContent.js
@@ -1,12 +1,19 @@
+// Import the custom hook 'useUser' from the hooks directory
 import { useUser } from "../../hooks/user";
 
-export default function RestrictedContent({ children }) { // propswithchildren
+// Define a functional component 'RestrictedContent' that takes 'children' as a prop
+export default function RestrictedContent({ children }) {
+    // Call the 'useUser' hook and destructure its returned values into 'user', 'fetching', and 'error'
     const { user, fetching, error } = useUser();
 
+    // Return a JSX fragment
     return (
         <>
-            {(user && user != null) ? children : null }
+            {/* If 'user' is truthy, render the 'children'; otherwise, render 'null' */}
+            {(user && user != null) ? children : null}
+            {/* If 'fetching' is true, render a paragraph with the text "Authorizing..."; otherwise, render 'null' */}
             {fetching ? <p>Authorizing...</p> : null}
+            {/* If 'error' is truthy, render a paragraph with the text "Error occurred: " followed by the error message; otherwise, render 'null' */}
             {error ? <p>Error occured: {error}</p> : null}
         </>
     )
